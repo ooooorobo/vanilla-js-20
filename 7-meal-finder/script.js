cd22ed95fb2ff8a62358ba007ad4e4f1f43ebced
ooooorobo <orobos654@gmail.com>
2021-10-25 02:45:27 +0900

    [Meal] add event listeners and stylesheet for single meal info

--- 7-meal-finder/script.js
+++ 7-meal-finder/script.js
@@ -46,5 +46,75 @@ async function searchMeal(e) {
     }
 }
 
+// Get random meal
+function getRandomMeal() {
+    // Clear meals and heading
+    mealsEl.innerHTML = ``
+    resultHeading.innerHTML = ``
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then(data => addMealToDOM(data.meals[0]))
+}
+
+// fetch meal by id
+function getMealById(mealID) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res => res.json())
+        .then(json => {
+            const meal = json.meals[0]
+            addMealToDOM(meal)
+        })
+}
+
+// Add meal to DOM
+function addMealToDOM(meal) {
+    const ingredients = []
+
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+        } else {
+            break
+        }
+    }
+
+    single_mealEl.innerHTML = `
+        <div class="single-meal">
+            <h1>${meal.strMeal}</h1>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+            <div class="single-meal-info">
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+            </div>
+            <div class="main">
+                <p>${meal.strInstructions}</p>
+                <h2>Ingredients</h2>
+                <ul>
+                    ${ingredients.map(i => `<li>${i}</li>`).join('')}
+                </ul>
+            </div>
+        </div>
+    `
+}
+
 // Event Listeners
 submit.addEventListener('submit', searchMeal)
+mealsEl.addEventListener('click',  e => {
+    e.preventDefault()
+    e.stopPropagation()
+
+    const mealInfo = e.path.find( item => {
+        if (item.classList) {
+            return item.classList.contains('meal-info')
+        } else {
+            return false
+        }
+    })
+
+    if (mealInfo) {
+        const mealID = mealInfo.getAttribute('data-mealid')
+        getMealById(mealID)
+    }
+})
+random.addEventListener('click', getRandomMeal)
