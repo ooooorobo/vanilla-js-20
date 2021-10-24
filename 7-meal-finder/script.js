032fa3a6926a937b97890bf8a0f48c5708325242
ooooorobo <orobos654@gmail.com>
2021-10-24 20:38:04 +0900

    [Meal] add search feature and meal styles

---
+++ 7-meal-finder/script.js
@@ -0,0 +1,50 @@
+// search meal by name - www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+// Lookup a single random meal - www.themealdb.com/api/json/v1/1/random.php
+
+const search = document.getElementById('search'),
+    submit = document.getElementById('submit'),
+    random = document.getElementById('random'),
+    mealsEl = document.getElementById('meals'),
+    resultHeading = document.getElementById('result-heading'),
+    single_mealEl = document.getElementById('single-meal')
+
+
+// Search meal and fetch from API
+async function searchMeal(e) {
+    e.preventDefault()
+
+    // Clear single meal
+    single_mealEl.innerHTML = ``
+
+    // Get the search term
+    const term = search.value
+
+    // Check for empty
+    if (term.trim()) {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+        const data = await res.json()
+
+        resultHeading.innerHTML = `
+            <h2>Search results for '${term}':</h2>
+        `
+        if (data.meals === null) {
+            resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
+        } else {
+            mealsEl.innerHTML = data.meals.map(meal => `
+                <div class="meal">
+                    <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+                    <div class="meal-info" data-mealID="${meal.idMeal}">
+                        <h3>${meal.strMeal}</h3>
+                    </div>
+                </div>
+            `).join('')
+        }
+
+        search.value = ''
+    } else {
+        alert('Please enter a search term')
+    }
+}
+
+// Event Listeners
+submit.addEventListener('submit', searchMeal)
